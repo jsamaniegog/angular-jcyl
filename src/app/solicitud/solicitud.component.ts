577903bb969c76041516ee1e4075149ed28fbf54
JAVIER SAMANIEGO GARCIA <jsamaniego@saludcastillayleon.es>
2022-04-25 22:00:47 +0200

    Ejercicio 2

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-solicitud',
   templateUrl: './solicitud.component.html',
-  styleUrls: ['./solicitud.component.css']
+  styleUrls: ['./solicitud.component.css'],
 })
 export class SolicitudComponent implements OnInit {
+  solicitud = {
+    nombre: 'Javier Samaniego García',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
